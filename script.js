a0500c8be570ae181de953752091c4cb62ad292c
Diego Tolentino <114973390+tolerevo@users.noreply.github.com>
2024-07-06 09:07:01 -0600

    add buttons and div to html

--- script.js
+++ script.js
@@ -1,65 +1,49 @@
 /*this function gets a random integer between 0 and 2 and assign the result to a string (rock,paper or 
 scissors)*/ 
-const paperBtn = document.createElement("button");
-paperBtn.setAttribute("id", "paperBtn");
+const paperBtn = document.getElementById("paperBtn");
 paperBtn.setAttribute("style", "width: 100px; height: 50px; background-color: #169FFF; padding: 20px; border-radius: 5px; color: white; font-size: 18px; font-weight: bold; justify-content: center; margin: 10px 10px 10px 20px; line-height: .3;");
 paperBtn.innerText = "paper";
-const scissorsBtn = document.createElement("button");
-scissorsBtn.setAttribute("id", "scissorsBtn");
+const scissorsBtn = document.getElementById("scissorsBtn");
 scissorsBtn.setAttribute("style", "width: 150px; height: 50px; background-color: #169FFF; padding: 20px; border-radius: 5px; color: white; font-size: 18px; font-weight: bold; justify-content: center; margin-right: 10px; line-height: .3;");
 scissorsBtn.innerText = "scissors";
-const rockBtn = document.createElement("button");
-rockBtn.setAttribute("id", "rockBtn");
+const rockBtn = document.getElementById("rockBtn");
 rockBtn.setAttribute("style", "width: 100px; height: 50px; background-color: #169FFF; padding: 20px; border-radius: 5px; color: white; font-size: 18px; font-weight: bold; justify-content: center; line-height: .3;");
 rockBtn.innerText = "rock";
-document.body.appendChild(paperBtn);
-document.body.appendChild(scissorsBtn);
-document.body.appendChild(rockBtn);
-const resultsDiv = document.createElement("div");
-resultsDiv.setAttribute("id", "resultsDiv");
+const resultsDiv = document.getElementById("resultsDiv");
 resultsDiv.setAttribute("style", "display: none;"); 
-document.body.appendChild(resultsDiv);
-const resultsP = document.createElement("p");
-resultsP.setAttribute("id", "resultsP");
+const resultsP = document.getElementById("resultsP");
 resultsP.setAttribute("style", "font-size: 22px; font-weight: bold; justify-content: center; line-height: 1.5; text-align: center; color: white;");
-resultsDiv.append(resultsP);
-
-/* This function gets the user's choice and returns it as a string, in lower case, only accepts: rock, paper or scissors as input.//
-function getUserChoice() {
-  let choice = prompt(
-    "Please choose 1 option: 'Rock', 'Paper' or 'Scissors': ",
-  ).toLowerCase();
-  if (choice!== "rock" && choice!== "paper" && choice!== "scissors") {
-    alert("Invalid choice");
-    return getUserChoice();
-  }
-  return choice;
-} */
+const resultsMessage = document.getElementById("resultsMessage");
+resultsMessage.setAttribute("style", "font-size: 22px; font-weight: bold; justify-content: center; line-height: 1.5; text-align: center; color: white;");
 
 /* game function compares the user's choice to the computer's choice and determines the winner, keeps the 
-count of round played and stops the game after 5 rounds and declares the winner. */
+count of round played and stops the game when a player reaches 5 points. */
 function game() {
   let round = 0;
   let humanScore = 0;
   let computerScore = 0;
-  // creates a new div referenced in the variable 'div'
-  // gives an id "results" to the div
-  // toggles the class "hide-results" to show the div and hides it when the game ends
-  //div.classList.toggle("display-results");
+
   /* play round function compares the user's choice to the computer's choice and applies the rules of the 
   game to decide the winner of a single round */
   function playRound(humanChoice, computerChoice) {
     if (humanChoice === computerChoice) {
-      console.log("Draw");
+      resultsMessage.innerText = "Draw";
     }
     if ((humanChoice === "rock" && computerChoice === "scissors") || (humanChoice === "scissors" && computerChoice === "paper") || (humanChoice === "paper" && computerChoice === "rock")) {
       humanScore++;
-      console.log(`You win! ${humanChoice} beats ${computerChoice}`);
+      resultsMessage.innerText = `You win! ${humanChoice} beats ${computerChoice}`;
     }
     if ((humanChoice === "scissors" && computerChoice === "rock") || (humanChoice === "paper" && computerChoice === "scissors") || (humanChoice === "rock" && computerChoice === "paper")) {
       computerScore++;
-      console.log(`You loss! ${computerChoice} beats ${humanChoice}`);
+      resultsMessage.innerText = `You loss! ${computerChoice} beats ${humanChoice}`;
       }
+    if(humanScore === 5){
+      resultsMessage.innerText = "You win!";
+      resetGame()
+    } else if (computerScore === 5){
+      resultsMessage.innerText = "You lose!";
+      resetGame();
+      }  
   }
 
 
@@ -96,34 +80,18 @@ function game() {
       let div = document.getElementById("resultsDiv");
       if (!div.classList.contains("display-results")) {
         div.classList.toggle("display-results");
-        div.setAttribute("style", "display: block; width: 100wh; height: 200px; background-color: #169FFF; padding: 5px; border-radius: 5px; justify-content: center; margin: 20px 10px; padding: 10px;");
+        div.setAttribute("style", "display: block; width: 100wh; height: 400px; background-color: #169FFF; padding: 5px; border-radius: 5px; justify-content: center; margin: 20px 10px; padding: 10px;");
       }
-    
-  document.getElementById("resultsP").innerText = (`Computer Chose: ${computerSelection} \r Player Chose: ${humanSelection} \r Player Points: ${humanScore} \r Computer Points: ${computerScore} \r Current Round: ${round}`); 
+      document.getElementById("resultsP").innerText = `Computer Chose: ${computerSelection} \r Player Chose: ${humanSelection} \r Player Points: ${humanScore} \r Computer Points: ${computerScore} \r Current Round: ${round}`; 
     // shows the results of the game in the div with id "results
     })
   })
-}
-
-game();
-    /*if (humanScore >= 5) {
-      console.log("You win!");
-      humanScore = 0;
-      } else if (computerScore >= 5){
-        console.log("You lose!");
-        computerScore = 0;
-      } */
-   /* here we use a while loop to set rounds to 5 and applies logic to play them, then logs the results of  the game and declares a winner or a draw. */
-  /*while (round < 5) {
-    let humanChoice = getUserChoice();
-    let computerChoice = getComputerChoice();
-    playRound(humanChoice, computerChoice);
-    round++;
-    console.log("Computer: " + computerChoice);
-    console.log("Player: " + humanChoice);
-    console.log("Player: " + humanScore);
-    console.log("Computer: " + computerScore);
-    console.log("Round: " + round);
+  const resetGame = () => {
+    humanScore = 0;
+    computerScore = 0;
+    round = 0;
+    document.getElementById("resultsP").setAttribute("style", "display: none;");
+    return game();
   }
-  console.log("Final Player Score: " + humanScore);
-  console.log("Final Computer Score: " + computerSChoice); */
+}
+game();
