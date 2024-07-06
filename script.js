919c2e51e17972b6f4904814972766db056920c5
Diego Tolentino <114973390+tolerevo@users.noreply.github.com>
2024-07-06 12:22:09 -0600

    add play again btn that resets the game

--- script.js
+++ script.js
@@ -1,20 +1,35 @@
 /*this function gets a random integer between 0 and 2 and assign the result to a string (rock,paper or 
-scissors)*/ 
+scissors)*/
 const paperBtn = document.getElementById("paperBtn");
-paperBtn.setAttribute("style", "width: 100px; height: 50px; background-color: #169FFF; padding: 20px; border-radius: 5px; color: white; font-size: 18px; font-weight: bold; justify-content: center; margin: 10px 10px 10px 20px; line-height: .3;");
+paperBtn.setAttribute(
+  "style",
+  "width: 100px; height: 50px; background-color: #169FFF; padding: 20px; border-radius: 5px; color: white; font-size: 18px; font-weight: bold; justify-content: center; margin: 10px 10px 10px 20px; line-height: .3;",
+);
 paperBtn.innerText = "paper";
 const scissorsBtn = document.getElementById("scissorsBtn");
-scissorsBtn.setAttribute("style", "width: 150px; height: 50px; background-color: #169FFF; padding: 20px; border-radius: 5px; color: white; font-size: 18px; font-weight: bold; justify-content: center; margin-right: 10px; line-height: .3;");
+scissorsBtn.setAttribute(
+  "style",
+  "width: 150px; height: 50px; background-color: #169FFF; padding: 20px; border-radius: 5px; color: white; font-size: 18px; font-weight: bold; justify-content: center; margin-right: 10px; line-height: .3;",
+);
 scissorsBtn.innerText = "scissors";
 const rockBtn = document.getElementById("rockBtn");
-rockBtn.setAttribute("style", "width: 100px; height: 50px; background-color: #169FFF; padding: 20px; border-radius: 5px; color: white; font-size: 18px; font-weight: bold; justify-content: center; line-height: .3;");
+rockBtn.setAttribute(
+  "style",
+  "width: 100px; height: 50px; background-color: #169FFF; padding: 20px; border-radius: 5px; color: white; font-size: 18px; font-weight: bold; justify-content: center; line-height: .3;",
+);
 rockBtn.innerText = "rock";
 const resultsDiv = document.getElementById("resultsDiv");
-resultsDiv.setAttribute("style", "display: none;"); 
+resultsDiv.setAttribute("style", "display: none;");
 const resultsP = document.getElementById("resultsP");
-resultsP.setAttribute("style", "font-size: 22px; font-weight: bold; justify-content: center; line-height: 1.5; text-align: center; color: white;");
+resultsP.setAttribute(
+  "style",
+  "font-size: 22px; font-weight: bold; justify-content: center; line-height: 1.5; text-align: center; color: white;",
+);
 const resultsMessage = document.getElementById("resultsMessage");
-resultsMessage.setAttribute("style", "font-size: 22px; font-weight: bold; justify-content: center; line-height: 1.5; text-align: center; color: white;");
+resultsMessage.setAttribute(
+  "style",
+  "font-size: 22px; font-weight: bold; justify-content: center; line-height: 1.5; text-align: center; color: white;",
+);
 
 /* game function compares the user's choice to the computer's choice and determines the winner, keeps the 
 count of round played and stops the game when a player reaches 5 points. */
@@ -29,23 +44,55 @@ function game() {
     if (humanChoice === computerChoice) {
       resultsMessage.innerText = "Draw";
     }
-    if ((humanChoice === "rock" && computerChoice === "scissors") || (humanChoice === "scissors" && computerChoice === "paper") || (humanChoice === "paper" && computerChoice === "rock")) {
+    if (
+      (humanChoice === "rock" && computerChoice === "scissors") ||
+      (humanChoice === "scissors" && computerChoice === "paper") ||
+      (humanChoice === "paper" && computerChoice === "rock")
+    ) {
       humanScore++;
       resultsMessage.innerText = `You win! ${humanChoice} beats ${computerChoice}`;
     }
-    if ((humanChoice === "scissors" && computerChoice === "rock") || (humanChoice === "paper" && computerChoice === "scissors") || (humanChoice === "rock" && computerChoice === "paper")) {
+    if (
+      (humanChoice === "scissors" && computerChoice === "rock") ||
+      (humanChoice === "paper" && computerChoice === "scissors") ||
+      (humanChoice === "rock" && computerChoice === "paper")
+    ) {
       computerScore++;
       resultsMessage.innerText = `You loss! ${computerChoice} beats ${humanChoice}`;
-      }
-    if(humanScore === 5){
+    }
+    if (humanScore === 5) {
       resultsMessage.innerText = "You win!";
-      resetGame()
-    } else if (computerScore === 5){
+      resetGame();
+    } else if (computerScore === 5) {
       resultsMessage.innerText = "You lose!";
       resetGame();
-      }  
+    }
   }
-
+  const resetGame = () => {
+    document.getElementById("resultsP").setAttribute("style", "display: none;");
+    const newGameBtn = document.getElementById("newGameBtn");
+    newGameBtn.setAttribute(
+      "style",
+      "width: 200px; height: auto; background-color: #169FFF; padding: 20px; border-radius: 5px; color: white; font-size: 18px; font-weight: bold; justify-content: center; line-height: .3;",
+    );
+    newGameBtn.innerText = "Play Again";
+    newGameBtn.addEventListener("click", () => {
+      humanScore = 0;
+      computerScore = 0;
+      round = 0;
+      const resultsP = document.getElementById("resultsP");
+      resultsP.setAttribute(
+        "style",
+        "display: block; font-size: 22px; font-weight: bold; justify-content: center; line-height: 1.5; text-align: center; color: white;",
+      );
+      resultsP.innerText = `Current Round: ${round} C'mon let's play!`;
+      newGameBtn.setAttribute(
+        "style",
+        "display: none; width: 0px; height: 0px;",
+      );
+      resultsMessage.innerText = "";
+    });
+  };
 
   function getComputerChoice() {
     function getRandomNum(min, max) {
@@ -59,18 +106,17 @@ function game() {
         ? choice[1]
         : choice[2];
   }
-// creating an event listeners so the buttons call playRound and gets user's choice every time they are clicked.
+  // creating an event listeners so the buttons call playRound and gets user's choice every time they are clicked.
   const buttons = document.querySelectorAll("button");
   buttons.forEach((button) => {
-    button.addEventListener("click", function() {
+    button.addEventListener("click", function () {
       getHumanChoice();
     });
 
-    const getHumanChoice = () => { 
+    const getHumanChoice = () => {
       let humanChoice = button.innerText;
-      return humanChoice
-    }
-
+      return humanChoice;
+    };
 
     button.addEventListener("click", () => {
       round = round + 1;
@@ -80,18 +126,15 @@ function game() {
       let div = document.getElementById("resultsDiv");
       if (!div.classList.contains("display-results")) {
         div.classList.toggle("display-results");
-        div.setAttribute("style", "display: block; width: 100wh; height: 400px; background-color: #169FFF; padding: 5px; border-radius: 5px; justify-content: center; margin: 20px 10px; padding: 10px;");
+        div.setAttribute(
+          "style",
+          "display: block; width: 100wh; height: auto; background-color: #169FFF; padding: 5px; border-radius: 5px; justify-content: center; margin: 20px 10px; padding: 10px;",
+        );
       }
-      document.getElementById("resultsP").innerText = `Computer Chose: ${computerSelection} \r Player Chose: ${humanSelection} \r Player Points: ${humanScore} \r Computer Points: ${computerScore} \r Current Round: ${round}`; 
-    // shows the results of the game in the div with id "results
-    })
-  })
-  const resetGame = () => {
-    humanScore = 0;
-    computerScore = 0;
-    round = 0;
-    document.getElementById("resultsP").setAttribute("style", "display: none;");
-    return game();
-  }
+      // shows the results of the game in the p tag with id "resultsP
+      document.getElementById("resultsP").innerText =
+        `Computer Chose: ${computerSelection} \r Player Chose: ${humanSelection} \r Player Points: ${humanScore} \r Computer Points: ${computerScore} \r Current Round: ${round}`;
+    });
+  });
 }
 game();
