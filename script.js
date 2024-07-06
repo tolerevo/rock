8b7ac6d929b519b725cf73ee2c123a516c6bdd65
Diego Tolentino <114973390+tolerevo@users.noreply.github.com>
2024-07-06 02:26:12 -0600

    mend

--- script.js
+++ script.js
@@ -1,17 +1,25 @@
-/*this function gets a random integer between 0 and 2 and assign the result to a string (rock,paper or 
+/* game function compares the user's choice to the computer's choice and determines the winner, keeps the 
+count of round played and stops the game after 5 rounds and declares the winner. */
+function playGame() {
+  let round = 0
+  let humanScore = 0
+  let computerScore = 0
+
+  /*this function gets a random integer between 0 and 2 and assign the result to a string (rock,paper or 
 scissors)*/ 
-function getComputerChoice() {
-  function getRandomNum(min, max) {
-    return Math.floor(Math.random() * max - min + 1) + min;
+  function getComputerChoice() {
+    function getRandomNum(min, max) {
+      return Math.floor(Math.random() * max - min + 1) + min;
+    }
+    let computerResponse = getRandomNum(0, 2);
+    let choice = ["rock", "paper", "scissors"];
+    return computerResponse === 0
+      ? choice[0]
+      : computerResponse === 1
+        ? choice[1]
+        : choice[2];
   }
-  let computerResponse = getRandomNum(0, 2);
-  let choice = ["rock", "paper", "scissors"];
-  return computerResponse === 0
-    ? choice[0]
-    : computerResponse === 1
-      ? choice[1]
-      : choice[2];
-}
+
 
 // This function gets the user's choice and returns it as a string, in lower case, only accepts: rock, paper or scissors as input.//
 function getHumanChoice() {
@@ -24,14 +32,6 @@ function getHumanChoice() {
   }
   return choice;
 }
-
-/* game function compares the user's choice to the computer's choice and determines the winner, keeps the 
-count of round played and stops the game after 5 rounds and declares the winner. */
-function playGame() {
-  let round = 0
-  let humanScore = 0
-  let computerScore = 0
-
   /* play round function compares the user's choice to the computer's choice and applies the rules of the 
   game to decide the winner of a single round */
   function playRound(humanChoice, computerChoice) {
@@ -52,17 +52,19 @@ function playGame() {
     }
   }
   /* here we use a while loop to set rounds to 5 and applies logic to play them, then logs the results of the game and declares a winner or a draw. */
-  while (round < 5) {
+  do {
+    round = round + 1;
     let humanSelection = getHumanChoice();
     let computerSelection = getComputerChoice();
     playRound(humanSelection, computerSelection);
-    round++;
     console.log("Computer Chose: " + computerSelection);
     console.log("Player Chose: " + humanSelection);
     console.log("Player Points: " + humanScore);
     console.log("Computer Points: " + computerScore);
     console.log("Current Round: " + round);
   }
+  while (round < 5); 
+
   console.log("Final Player Score: " + humanScore);
   console.log("Final Computer Score: " + computerScore);
   
